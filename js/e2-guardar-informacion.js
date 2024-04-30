ea0942b35afc5da88b4a5588513b566b5649635e
Bootcamp QA <mybootcampqa@gmail.com>
2024-04-30 20:14:21 +0200

    ejercicio 6 guardar informacion

--- js/e2-guardar-informacion.js
+++ js/e2-guardar-informacion.js
@@ -203,3 +203,138 @@ document.write("El libro se publicó en " + publicacion + "</br>");
 document.write("El libro tiene "  + numeroPaginas +" " + "páginas " + "</br>");
 document.write("¿El libro tiene película? "+ tienePelicula + "</br>");
 document.write("------------------------------------------------------------------------------------------" + "</br>");
+
+document.write("<h4>EJERCICIO 5: ASIGNAR VALORES A VARIABLES CON DATOS DE DEPORTES Y CAMBIARSELOS </h4>");
+let nombreDeporte, lugarDeporte, popularidadDeporte, equipamientoDeporte, esDeporteEquipo, esDeporteExterior;
+nombreDeporte = "Voleibol";
+lugarDeporte = "Estados Unidos";
+popularidadDeporte = 8;
+equipamientoDeporte = "Red y pelota";
+esDeporteEquipo = true;
+esDeporteExterior = true;
+
+document.write("<p>1. Informacion sobre el deporte: <b>" + nombreDeporte + "</b></p>");
+document.write("<p>· Lugar de origen = " + lugarDeporte + "</p>");
+document.write("<p>· Popularidad = " + popularidadDeporte + "</p>");
+document.write("<p>· Equipamiento necesario = " + equipamientoDeporte + "</p>");
+document.write("<p>· ¿Es un deporte de equipo? = " + esDeporteEquipo + "</p>");
+document.write("<p>· ¿Se práctica en exteriores? = " + esDeporteExterior + "</p>");
+
+//Cambiar valores y volverlos a mostrar por pantalla
+nombreDeporte = "Natación";
+lugarDeporte = "Gran Bretaña";
+popularidadDeporte = 9;
+equipamientoDeporte = "Bañador, gorro y gafas";
+esDeporteEquipo = true;
+esDeporteExterior = true;
+
+document.write("<p>2. Informacion sobre el deporte: <b>" + nombreDeporte + "</b></p>");
+document.write("<p>· Lugar de origen = " + lugarDeporte + "</p>");
+document.write("<p>· Popularidad = " + popularidadDeporte + "</p>");
+document.write("<p>· Equipamiento necesario = " + equipamientoDeporte + "</p>");
+document.write("<p>· ¿Es un deporte de equipo? = " + esDeporteEquipo + "</p>");
+document.write("<p>· ¿Se práctica en exteriores? = " + esDeporteExterior + "</p>");
+document.write("<hr>");
+
+document.write("</br> EJERCICIO  6 - DECLARAR VARIABLES PARA VIAJES. DESTINO DE VIAJE FAVORITO</br>");
+
+let destination = "Kioto";
+let country = "Japón";
+let predominantClimate = "Damp";
+let population = 1465000;
+let touristAttraction = "Fushimi Inari-Taisha (Santuario principal al dios Inari) Templo del Pabellón de Oro (Kinkaku-ji), Kiyomizu-dera (Templo del agua pura) el mercado del distrito de Nishiki, Eikan-do (templo con unos jardines perfectos), Castillo Nijo";
+let coastalDestination = true;
+let urbanDestination = true;
+document.write("<br>");
+document.write("Nombre del destino:" + destination + "<br>");
+document.write("Nombre país:" + country + "<br>");
+document.write("¿Cuál es el clima predominante?:" + predominantClimate + "<br>");
+document.write("¿Cuál es su población?:" + population + "<br>");
+document.write("Principales atracciones turísticas:" + touristAttraction + "<br>");
+document.write("¿Es un destino costero?:" + coastalDestination + "<br>");
+document.write("Es un destino urbano?:" + urbanDestination + "<br>");
+
+
+document.write("</br> EJERCICIO  6.1 - CAMBIAR VARIABLES CON DESTINO MÁLAGA</br>");
+
+destination = "Málaga";
+country = "España";
+predominantClimate = "Soleado";
+population = 579076;
+touristAttraction = "Fortaleza de la Alcazaba, Catedral de Málaga, Castillo de Gibralfaro, Puerto de Málaga, Calle Larios, Plaza de la Constitución, \ Teatro Romano, Mirador del Gibralfaro, Basílica de Santa María de la Victoria, Iglesia de los Mártires.";
+
+
+document.write("<br>");
+document.write("Nombre del destino:" + destination + "<br>");
+document.write("Nombre país:" + country + "<br>");
+document.write("¿Cuál es el clima predominante?:" + predominantClimate + "<br>");
+document.write("¿Cuál es su población?:" + population + "<br>");
+document.write("Principales atracciones turísticas:" + touristAttraction + "<br>");
+document.write("¿Es un destino costero?:" + coastalDestination + "<br>");
+document.write("Es un destino urbano?:" + urbanDestination + "<br>");
+
+document.write("</br> EJERCICIO  6 VERSION 2</br>");
+
+let destino; 
+let pais; 
+let clima; 
+let poblacion;
+let atraccionesTuristicas;
+let destinoCostero= false;
+let destinoUrbano = false; 
+
+destino = "Madeira"; 
+pais = "Portugal";
+clima = "Mediterráneo";
+poblacion = 10000000;
+atraccionesTuristicas = "cultura, gastronomía, etc.";
+ 
+
+
+document.write("Destino: " +destino);
+document.write("</br>");
+
+document.write("País: " + pais);
+document.write("</br>");
+
+document.write("Clima: " + clima);
+document.write("</br>");
+
+document.write("Poblacion: " + poblacion + " de habitantes");
+document.write("</br>");
+
+document.write("Atracciones turísticas: " + atraccionesTuristicas);
+document.write("</br>");
+
+document.write("Es destino costero: " + destinoCostero);
+document.write("</br>");
+
+document.write("Es destino urbano: " + destinoUrbano);
+document.write("</br>");
+
+destino = "Malaga"; 
+pais = "España"; 
+clima = "Mediterráneo";
+poblacion = 500000;
+atraccionesTuristicas = "cultura, gastronomía, etc.";
+destinoUrbano =true;
+
+document.write("Destino: " + destino);
+document.write("</br>");
+
+document.write("País: " + pais);
+document.write("</br>");
+
+document.write("Clima: " + clima);
+document.write("</br>");
+
+document.write("Poblacion: " + poblacion + " habitantes");
+document.write("</br>");
+
+document.write("Atracciones turísticas: " + atraccionesTuristicas);
+document.write("</br>");
+document.write("Es destino costero: " + destinoCostero);
+document.write("</br>");
+
+document.write("Es destino urbano: " + destinoUrbano);
+document.write("</br>");
