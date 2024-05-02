9b59126711792f8df108d0371f59ff3eefdd5315
Bootcamp QA <mybootcampqa@gmail.com>
2024-05-02 19:15:39 +0200

    ejercicio 7 constantes

--- js/e2-ejercicios-guardar-informacion.js
+++ js/e2-ejercicios-guardar-informacion.js
@@ -285,3 +285,31 @@ document.write("</br>");
 
 document.write("Es destino urbano: " + destinoUrbano);
 document.write("</br>");
+
+//EJERCICIO 7 CONSTANTES
+document.write("EJERCICIO 7: EJERCICIOS DE CONSTANTES:</br>");
+const NUMEROMESESAÑO = 12;
+const  NUMERODEDIASSEMANADOS =7;
+let numeroHabitantescuidad;
+let edadCantante;
+const  ESTACIONESAÑO = 4;
+let cuidadVivo;
+const NUMEROHORASDIA = 24;
+let precioGasolina;
+let numeroLibros;
+
+numeroHabitantescuidad = 3000000;
+edadCantante = 40;
+cuidadVivo = "Madrid";
+precioGasolina = 1.75;
+numeroLibros = 50;
+
+document.write("El año tiene " + NUMEROMESESAÑO + " meses" + "</br>");
+document.write("La semana tiene " + NUMERODEDIASSEMANADOS + " días" + "</br>");
+document.write(" Número de habitantes de la cuidad: " + numeroHabitantescuidad + " habitantes" +"</br>");
+document.write(" Edad cantante favorito: " + edadCantante + " años" +"</br>");
+document.write("El año tiene " + ESTACIONESAÑO + " estaciones" +"</br>");
+document.write("Vivo en  " + cuidadVivo +"</br>");
+document.write("El día tiene " + NUMEROHORASDIA + " horas" + "</br>");
+document.write("La gasolina cuesta " + precioGasolina + " euros" + "</br>");
+document.write("He leído " + numeroLibros + " libros" + "</br>");
